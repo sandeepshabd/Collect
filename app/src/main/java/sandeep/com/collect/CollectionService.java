71eadeafcedb071718aa2ee90dfc903e6a174fe7
Sandeep Shabd <sandeep.shabd@gm.com>
2015-01-18 23:37:05 -0500

    changes to fix the scheduled task

--- app/src/main/java/sandeep/com/collect/CollectionService.java
+++ app/src/main/java/sandeep/com/collect/CollectionService.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -25,23 +23,34 @@ public class CollectionService extends Service {
     private static final String TAG="CollectionService";
 
     boolean collectionStarted = false;
+
     long COLLECTION_TIME_MS = 10000; // 10 seconds
     long SEND_TIME_MS = 60000; // 1 minute
+
     int START_MORNING_HOUR= 8; //8 AM
+    int START_MORNING_MINUTE= 0; //8 AM
+    int START_MORNING_SECONDS= 0; //8 AM
+    int START_MORNING_MILLIS= 0; //8 AM
+
     int END_EVENING_HOUR= 18; // 6 PM
+    int END_EVENING_MINUTE= 0;
+    //int TEST_TIME= 18;
+
+    long SCHEDULEDING_TIME_IN_MS = 1000 * 60 * 60 * 24 * 7;
+    //long SCHEDULEDING_TIME_IN_MS = 1000*60*4;
+
     private SensorManager mSensorManager;
     Sensor accelerometer;
     Sensor magnetometer;
     Context context =this;
-    Timer scheduleTimer ;
+    Timer scheduleTimer = new Timer();
     Timer dailyRunTimer ;
     StringBuilder resultMaker = new StringBuilder();
     Calendar mainCalendar = new GregorianCalendar();
-    TimerTask doAsynchronousTask;
-    TimerTask sendDataTask;
     TimerTask scheduleDataTask;
-
-
+    SendTask sendDataTask ;
+    CollectionAsyncTask collectionAsyncTask;
+    final CollectionTask collectionTask = new CollectionTask();
 
     public CollectionService() {}
 
@@ -55,78 +64,61 @@ public class CollectionService extends Service {
         mSensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_UI);
         mSensorManager.registerListener(sensorListener, magnetometer, SensorManager.SENSOR_DELAY_UI);
 
-        //Async task to gather data
-        final CollectionTask collectionTask = new CollectionTask();
-
-        //data collection task
-        doAsynchronousTask = new TimerTask() {
-            public void run() {
-                collectionTask.execute();
-                resultMaker.append("{");
-                resultMaker.append(DataClass.singleRunResult);
-                resultMaker.append("},");
-
-                int hour = mainCalendar.get( Calendar.HOUR_OF_DAY );
-                if(hour>END_EVENING_HOUR){
-                    sendDataTask.cancel();
-                    sendTask();
-                    doAsynchronousTask.cancel();
-                    scheduleTimer.cancel();
-                    scheduleTimer.purge();
-                    scheduleTimer=null;
-                }
-            }
-        };
-
-
-        //data send task
-        sendDataTask = new TimerTask() {
-            public void run() {
-                sendTask();
-            }
-        };
-
         //daily run task
         scheduleDataTask = new TimerTask() {
             public void run() {
-                Timer newTime =  new Timer();
-                startCollectingSendingData(newTime);
-            }
+                Log.i(TAG,"Scheduled data sending task for everyday.");
+               // TEST_TIME= TEST_TIME+3; -- for test runs
+               // END_EVENING_MINUTE=TEST_TIME; -- for test runs
+                startCollectingSendingData();
+           }
         };
 
-
-        int hour = mainCalendar.get( Calendar.HOUR_OF_DAY );
-        Log.i(TAG,"hour of day:"+hour);
+        mainCalendar = new GregorianCalendar();
+        int current_hour = mainCalendar.get( Calendar.HOUR_OF_DAY );
+        int current_minute = mainCalendar.get( Calendar.MINUTE );
+        Log.i(TAG,"hour of day:"+current_hour+":"+current_minute);
+        Log.i(TAG,"START TIME:"+START_MORNING_HOUR+":"+START_MORNING_MINUTE+" END TIME:"+END_EVENING_HOUR+":"+END_EVENING_MINUTE);
         //process to start only between 8 AM and 6 PM
-        scheduleDailyRun();
-        if(hour >= START_MORNING_HOUR && hour <END_EVENING_HOUR){
-            Log.i(TAG,"start collection");
+
+        if((current_hour >= START_MORNING_HOUR && current_minute >= START_MORNING_MINUTE)
+                && (current_hour <= END_EVENING_HOUR && current_minute < END_EVENING_MINUTE)){
+            Log.i(TAG,"start collection now as it is within the time range.");
             collectionStarted = true;
-            Timer newTime =  new Timer();
-            startCollectingSendingData(newTime);
+            startCollectingSendingData();
+        }else{
+            Log.i(TAG,"Not running the task as current time is outside range.");
         }
+        dailyRunTimer= new Timer();
+        scheduleDailyRun();
     }
 
     private void scheduleDailyRun(){
-        Log.i(TAG,"scheduleDailyRun");
-        dailyRunTimer = new Timer();
-        Calendar date = Calendar.getInstance();
-        date.set(
-                Calendar.DAY_OF_WEEK,
-                Calendar.SUNDAY
-        );
-        date.set(Calendar.HOUR, START_MORNING_HOUR);
-        date.set(Calendar.MINUTE, 0);
-        date.set(Calendar.SECOND, 0);
-        date.set(Calendar.MILLISECOND, 0);
+
+        Calendar date = new GregorianCalendar();
+        int nowHour= date.get(Calendar.HOUR_OF_DAY);
+        if(nowHour >=START_MORNING_HOUR ){
+            date.add(Calendar.DATE, 1);
+        }
+
+        date.set(Calendar.HOUR_OF_DAY, START_MORNING_HOUR);
+        date.set(Calendar.MINUTE, START_MORNING_MINUTE);
+
+        //date.set(Calendar.HOUR_OF_DAY, 23);-- test runs
+       // date.set(Calendar.MINUTE, TEST_TIME);-- test runs
+
+        date.set(Calendar.SECOND, START_MORNING_SECONDS);
+        date.set(Calendar.MILLISECOND, START_MORNING_MILLIS);
+
+        Log.i(TAG,"scheduleDailyRun - daily run. Start:"+ date.get(Calendar.DATE)+"-"+date.get(Calendar.HOUR_OF_DAY)+":"+date.get(Calendar.MINUTE)+" Every ms:"+SCHEDULEDING_TIME_IN_MS);
+
         dailyRunTimer.schedule(scheduleDataTask,
-                date.getTime(),
-                1000 * 60 * 60 * 24 * 7
+                date.getTime(),SCHEDULEDING_TIME_IN_MS
         );
     }
 
     private void sendTask(){
-        Log.i(TAG,"sendTask");
+        Log.i(TAG,"sendTask function");
         String result = "["+resultMaker.toString()+
                         "{"+DataClass.singleRunResult+"}]";
         DataClass.singleRunResult="";
@@ -137,17 +129,24 @@ public class CollectionService extends Service {
             JSONArray jsnobject = new JSONArray(result);
             RestClient.get().postCollectionData(jsnobject);
         }catch(Exception ex){
-           // Log.e(TAG,ex.getMessage());
-            ex.printStackTrace();
+           Log.e(TAG,ex.getMessage());
         }
 
     }
 
-    private void startCollectingSendingData(Timer timeToRun){
-        Log.i(TAG,"startCollectingSendingData");
-        scheduleTimer= timeToRun;
-        scheduleTimer.schedule(doAsynchronousTask, 0, COLLECTION_TIME_MS);
-        scheduleTimer.schedule(sendDataTask, 0, SEND_TIME_MS);
+    private void startCollectingSendingData(){
+        Log.i(TAG,"startCollectingSendingData function call. ");
+
+        try {
+            scheduleTimer = new Timer();
+            //data send task
+            sendDataTask = new SendTask();
+            collectionAsyncTask = new CollectionAsyncTask();
+            scheduleTimer.schedule(collectionAsyncTask, 0, COLLECTION_TIME_MS);
+            scheduleTimer.schedule(sendDataTask, 0, SEND_TIME_MS);
+        }catch(Exception e){
+            Log.e(TAG,"Timer task is already scheduled.");
+        }
     }
 
     @Override
@@ -170,12 +169,45 @@ public class CollectionService extends Service {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
+    private class CollectionAsyncTask extends TimerTask{
+        public void run() {
+            Log.i(TAG,"doAsynchronousTask to collect the data.");
+            collectionTask.execute();
+            resultMaker.append("{");
+            resultMaker.append(DataClass.singleRunResult);
+            resultMaker.append("},");
+            mainCalendar = new GregorianCalendar();
+            int hour = mainCalendar.get( Calendar.HOUR_OF_DAY );
+            int minute =mainCalendar.get( Calendar.MINUTE);
+            Log.i(TAG,"current time:"+hour +":"+minute);
+            Log.i(TAG,"Stopping time:"+END_EVENING_HOUR+":"+END_EVENING_MINUTE);
+            if(hour>=END_EVENING_HOUR && minute>=END_EVENING_MINUTE){
+                Log.i(TAG, "Time is past the collection time. Should stop collection.");
+                sendDataTask.cancel();
+                sendTask();
+                this.cancel();
+                scheduleTimer.cancel();
+                scheduleTimer.purge();
+
+                scheduleTimer=null;
+                //collectionAsyncTask= null;
+                sendDataTask=null;
+                Log.i(TAG,"Everything stopped for today.");
+            }
+        }
+    }
+
+    private class SendTask extends TimerTask{
+        public void run() {
+            Log.i(TAG,"Scheduled send task");
+            sendTask();
+        }
+    }
+
     private class CollectionTask {
 
         DateFormat timeFormat = new SimpleDateFormat("yyyyy-mm-dd HH:mm:ss:SSSZ");
         //get current date time with Date()
-
-
 
          protected void execute(){
              Log.i(TAG,"start execute collection service");
